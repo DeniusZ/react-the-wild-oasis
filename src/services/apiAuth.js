8ff4d52cb77bc4def71c5ecdb23000967d535221
Deniss Zubkovs <deniss.zubkovs@issuu.com>
2025-06-21 08:07:41 +0200

    add ability to update user name, password and add avatar

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,5 @@
-import supabase from "./supabase";
+/* eslint-disable no-unused-vars */
+import supabase, { supabaseUrl } from "./supabase";
 
 export const signup = async ({ fullName, email, password }) => {
   const { data, error } = await supabase.auth.signUp({
@@ -42,4 +43,33 @@ export const getCurrentUser = async () => {
 export const logout = async () => {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+};
+
+export const updateCurrentUser = async ({ password, fullName, avatar }) => {
+  // 1. update password OR full name
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  // 2. upload avatar
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. update avatar in the user
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (error2) throw new Error(error2.message);
+  return updatedUser;
 };
